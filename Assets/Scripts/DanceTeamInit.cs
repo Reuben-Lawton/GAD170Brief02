7c52cc912d15062dff79ad77a9808497b261d375
Reuben-Lawton <85323219+Reuben-Lawton@users.noreply.github.com>
2021-07-25 02:35:57 +1000

    redid the dance team init

--- Assets/Scripts/DanceTeamInit.cs
+++ Assets/Scripts/DanceTeamInit.cs
@@ -15,7 +15,7 @@ public class DanceTeamInit : MonoBehaviour
     public int dancersPerSide = 3; // This is the number of dancers for each team, if you want more, you need to modify this in the inspector.
     public CharacterNameGenerator nameGenerator; // This is a reference to our CharacterNameGenerator instance.
     private CharacterName[] teamACharacterNames; // An array to hold all our character names of TeamA.
-    private CharacterName[] teamBcharacterNames; // An array to hold all the character names of TeamB
+    private CharacterName[] teamBCharacterNames; // An array to hold all the character names of TeamB
 
     /// <summary>
     /// Called to iniatlise the dance teams with some dancers :D
@@ -24,9 +24,28 @@ public class DanceTeamInit : MonoBehaviour
     {
         Debug.LogWarning("InitTeams called, needs to generate names for the teams and set them with teamA.SetTroupeName and teamA.InitialiseTeamFromNames");
         // We need to set out team names using teamA.SetTroupeName.
+
+        teamA.SetTroupeName("The Misfits");
         // We need to generate some character names for our teams to use from our CharacterNameGenerator.
+
+        nameGenerator.ReturnTeamCharacterNames(dancersPerSide);
         // We need to spawn in some dancers using teamA.InitialiseTeamFromNames.
 
-        Debug.LogWarning("InitTeams called, needs to create character names via CharacterNameGenerator and get them into the team.InitaliseTeamFromNames");
+        teamACharacterNames = nameGenerator.ReturnTeamCharacterNames(dancersPerSide);
+
+        teamA.InitaliseTeamFromNames(dancerPrefab, DanceTeam.Direction.Left, teamACharacterNames);
+
+        teamB.SetTroupeName("Leaping Lizards");
+        // We need to generate some character names for our teams to use from our CharacterNameGenerator.
+
+        nameGenerator.ReturnTeamCharacterNames(dancersPerSide);
+        // We need to spawn in some dancers using teamB.InitialiseTeamFromNames.
+
+        teamBCharacterNames = nameGenerator.ReturnTeamCharacterNames(dancersPerSide);
+
+
+        teamB.InitaliseTeamFromNames(dancerPrefab, DanceTeam.Direction.Left, teamBCharacterNames);
+
+        Debug.LogWarning("InitTeams called, have created characters via CharacterNameGenerator and get them into the team.InitaliseTeamFromNames");
     }
 }
